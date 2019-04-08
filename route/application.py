d12cc9348dbf7cfbfd1a11de7e4eacecb402c7a3
Teresa Cunningham <cunningthu@gmail.com>
2019-04-07 19:45:32 -0700

    testing flask

---
+++ route/application.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+
+def index():
+    return "Hello, world!!!"
+
+@app.route("/<string:name>")
+def hello(name):
+    name = name.capitalize()
+    return f"Hello, {name}!"
