d6ca74bda793e981fe60cfabb5e1f10c6b969803
Teresa Cunningham <cunningthu@gmail.com>
2019-04-21 09:14:52 -0700

    adding flights html page to view flights and flight html for flight info

--- database/application.py
+++ database/application.py
@@ -11,8 +11,8 @@ db = scoped_session(sessionmaker(bind=engine))
 
 @app.route("/")
 def index():
-    flights = db.execute("SELECT * FROM flights").fetchall();
-    return render_template("index.html", flights=flights);
+    flights = db.execute("SELECT * FROM flights").fetchall()
+    return render_template("index.html", flights=flights)
 
 @app.route("/book", methods=["POST"])
 def book():
@@ -32,3 +32,23 @@ def book():
         {"name":name, "flight_id":flight_id})
     db.commit();
     return render_template("success.html")
+
+@app.route("/flights")
+def flights():
+    """Lists all flights."""
+    flights = db.execute("SELECT * FROM flights").fetchall()
+    return render_template("flights.html", flights=flights)
+
+@app.route("/flights/<int:flight_id>")
+def flight(flight_id):
+    """Lists details about a single flight."""
+
+    # Make sure flight exists.
+    flight = db.execute("SELECT * FROM flights WHERE id = :id", {"id":flight_id}).fetchone();
+    if flight is None:
+        return render_template("error.html", message="No such flight.")
+
+    # Get all passenger.
+    passengers = db.execute("SELECT name FROM passengers WHERE flight_id=:flight_id",
+        {"flight_id":flight_id}).fetchall()
+    return render_template("flight.html", flight=flight, passengers=passengers)
