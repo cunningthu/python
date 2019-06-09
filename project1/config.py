fc196e26e010db81ca4d0be8f96b2cf11694f479
Teresa Cunningham <cunningthu@gmail.com>
2019-06-09 12:55:22 -0700

    oops

--- project1/config.py
+++ project1/config.py
@@ -3,5 +3,5 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = 'postgresql+pg8000://Feeva:***REMOVED***@localhost:5432/bookreview'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+pg8000://Feeva:password@localhost:5432/bookreview'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
