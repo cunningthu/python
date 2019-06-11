4cd6eba29554b0393c488e15a4b09f6fc15f9d2e
Teresa Cunningham <cunningthu@gmail.com>
2019-06-11 07:11:10 -0700

    oops

--- OOP/classes/application.py
+++ OOP/classes/application.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request
 from models import *
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = 'postgresql+pg8000://Feeva:***REMOVED***@localhost:5432/oop'
+app.config["SQLALCHEMY_DATABASE_URI"] = 'postgresql+pg8000://Feeva:password@localhost:5432/oop'
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db.init_app(app)
 
